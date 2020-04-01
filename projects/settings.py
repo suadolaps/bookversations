28a4aa77305b923045fd5d419d6a3d69df297c22
suadolaps <suadkamardeen@gmail.com>
2020-04-01 12:38:57 +0200

    Add requirements file

--- projects/settings.py
+++ projects/settings.py
@@ -66,8 +66,12 @@ WSGI_APPLICATION = 'projects.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'bookversations',
+        'USER': 'bookversationsadmin',
+        'PASSWORD': 'Orisuad2018',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
